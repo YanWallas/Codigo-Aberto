423078551ad4ad2aa3efc3b03f222e0f10facedc
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-11-22 11:34:13 -0300

    Gerador de CPF

---
+++ Gerador de CPF/assets/js/GeraCPF.js
@@ -0,0 +1,33 @@
+class GerarCPF {
+    rand(min = 100000000, max = 999999999) {
+        return String(Math.floor(Math.random() * (max - min) + min));
+    }
+
+    formatado(cpf) {
+        return (
+            cpf.slice(0, 3) + '.' +
+            cpf.slice(3, 6) + '.' +
+            cpf.slice(6, 9) + '-.' +
+            cpf.slice(9, 11)
+        );
+    }
+
+    geraNovoCpf() {
+        const cpfSemDigito = this.rand();
+        const digito1 = ValidaCPF.geraDigito(cpfSemDigito);
+        const digito2 = ValidaCPF.geraDigito(cpfSemDigito + digito1);
+        const novoCPF = cpfSemDigito + digito1 + digito2;
+        return this.formatado(novoCPF);
+    }
+}
+const GCPF = document.querySelector('.GCPF');
+
+function click() {
+    GCPF.addEventListener('click', ()=> {
+        const gera = new GerarCPF();
+        const cpfGerado = document.querySelector('.cpf-gerado');
+        cpfGerado.innerHTML = gera.geraNovoCpf();
+    })
+}
+
+click();
